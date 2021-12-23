62ada60f50fff4aba368f9b206b0a58c15d900d6
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-23 19:37:17 +0900

    [setting] 에러해결 ts 환경 설정 / babel 설정 웹팩/타입스트립트 작동 잘됨

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'none',
-  entry: './src/index.js',
+  entry: './src/index.ts',
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -15,6 +16,25 @@ module.exports = {
     new HtmlWebpackPlugin({
       // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
       template: 'public/index.html',
+
     }),
+
   ],
+
+  module: {
+    // 모듈 규칙
+    rules: [
+      // TypeScript 로더 설정
+      {
+        test: /\.tsx?$/i,
+        exclude: /node_modules/,
+        use: ['ts-loader']
+      }
+    ]
+  },
+
+  resolve: {
+    // 생략 가능한 확장자로`.ts`, `.tsx` 추가
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  }
 };
