3ba4fe92f1db42c3b42ced9f203c6b2940af94a4
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-24 14:45:52 +0900

    [test] App, RandomNumber 테스트코드 작성

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('App', () => {
-  const { getByText } = render(<App />);
-  expect(getByText('안녕')).not.toBeNull();
+jest.mock('react-redux');
+
+test('render App', () => {
+  const { getByText, getByDisplayValue, getByPlaceholderText } = render(<App />);
+  expect(getByText('이룸청년 어른이들을 위한 크리스마스 선물')).not.toBeNull();
+  expect(getByPlaceholderText('시작 숫자 입력')).not.toBeNull();
+  expect(getByPlaceholderText('끝 숫자 입력')).not.toBeNull();
+  expect(getByText('숫자 뽑기')).not.toBeNull();
+});
+
+describe('Main', () => {
+  it('input works', () => {
+    // useSelector.mockImplementation(selector)
+
+  });
 });
---
+++ src/rotues/RandomNumber.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+const { default: RandomNumber } = require('./RandomNumber');
+
+test('RandomNumber', () => {
+  const { getByText, } = render(<RandomNumber />);
+  expect(getByText('숫자 뽑기'));
+  expect(getByText('리셋'));
+  expect(getByText('1'))
+  expect(getByText('100'))
+});
